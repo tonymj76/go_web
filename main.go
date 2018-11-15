17078d1cbe84df74a422bf31252031939817dd03
Anthony <demeyortj@gmail.com>
2018-11-15 17:01:53 +0100

    how to defind a custom handler

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 type myHandler struct{}
 
+// anything that has ServeHTTP is an handler
 func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Tony's new go web app %v", r.URL.Path[1:])
 
@@ -14,6 +15,9 @@ func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	handler := myHandler{}
+	
+	// Here we defind a default handler which will be the same
+	// all the page you visit
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: &handler,
