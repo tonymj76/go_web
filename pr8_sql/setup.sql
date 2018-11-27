b222820a451fbb7a9f8c76c55eff16f901104ad3
Anthony <demeyortj@gmail.com>
2018-11-27 16:14:00 +0100

    added comment and using manual setup database without 3rd party

--- pr8_sql/setup.sql
+++ pr8_sql/setup.sql
@@ -1,5 +1,16 @@
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS comments;
+
+
 CREATE TABLE posts (
    id serial PRIMARY KEY,
-   content TEXT,
+   content TEXT NOT NULL,
    author varchar(255)
-)
+);
+
+CREATE TABLE comments (
+   commentsId serial PRIMARY KEY, -- primary key column
+   content TEXT NOT NULL,
+   author VARCHAR(255) NOT NULL,
+   post_id INTEGER REFERENCES posts(id)
+);
--- pr8_sql/store.go
+++ pr8_sql/store.go
@@ -3,20 +3,47 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/pkg/errors"
+	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 type Post struct {
-	ID      int
-	Content string
-	Author  string
+	ID       int
+	Content  string
+	Author   string
+	Comments []Comment
 }
 
+type Comment struct {
+	CommentID int
+	Content   string
+	Author    string
+	P         *Post
+}
+
+// DB is the database
 var DB *sql.DB
+
+func check(e error) {
+	if e != nil {
+		log.Println(e)
+	}
+}
 
 // connecting with DB
 func init() {
+	file, errs := os.OpenFile("gwp.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	log.SetOutput(file)
+	log.SetPrefix("TRACE")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	if errs != nil {
+		panic(errs)
+	}
+	defer file.Close()
+
 	var err error
 	DB, err = sql.Open("postgres", "user=tonymj dbname=gwp password=t sslmode=disable")
 	if err != nil {
@@ -24,7 +51,8 @@ func init() {
 	}
 }
 
-func Posts(limit int) (posts []Post, err error) {
+// GetPosts return query given by limit and go tro each row and asign it to the struct
+func GetPosts(limit int) (posts []Post, err error) {
 	rows, err := DB.Query("SELECT id, content, author FROM posts LIMIT $1", limit)
 	if err != nil {
 		return
@@ -44,12 +72,25 @@ func Posts(limit int) (posts []Post, err error) {
 // GetPost Gets a single post
 func GetPost(id int) (post Post, err error) {
 	post = Post{}
+	post.Comments = []Comment{}
 	err = DB.QueryRow("SELECT id, content, author FROM posts where id = $1", id).Scan(&post.ID, &post.Content, &post.Author)
+
+	rows, err := DB.Query("SELECT commentsid, content, author FROM comments")
+	defer rows.Close()
+	check(err)
+	for rows.Next() {
+		comm := Comment{P: &post}
+		err = rows.Scan(&comm.CommentID, &comm.Content, &comm.Author)
+		check(err)
+		post.Comments = append(post.Comments, comm)
+	}
 	return
 }
 
 // Create creates a new post
 func (post *Post) Create() (err error) {
+	// return id here returns the id in the sql and adds it to the post.id
+	// this return the id of this particular insert which is den added to post.ID
 	statement := "INSERT INTO posts (content, author) VALUES ($1, $2) returning id"
 	stmt, err := DB.Prepare(statement)
 	if err != nil {
@@ -57,6 +98,8 @@ func (post *Post) Create() (err error) {
 		return
 	}
 	defer stmt.Close()
+	// .Scan takes the returned id and assign it to addr of post.ID
+	// where post.Content and Author are the value we are adding to the db
 	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.ID)
 	return
 }
@@ -73,6 +116,21 @@ func (post *Post) Delete() (err error) {
 	return
 }
 
+func (post Post) String() string {
+	return fmt.Sprintf("%v: %s \t%s\n", post.ID, post.Author, post.Content)
+}
+
+// Create Comment
+func (comment *Comment) Create() (err error) {
+	if comment.P == nil {
+		err = errors.New("Post not found")
+		return
+	}
+	err = DB.QueryRow("INSERT INTO comments (content, author, post_id) VALUES ($1, $2, $3) returning commentsId", comment.Content, comment.Author, comment.P.ID).Scan(&comment.CommentID)
+	check(err)
+	return
+}
+
 func main() {
 	post := Post{Content: "Hello world", Author: "tony"}
 	post2 := Post{Content: "Hello tom", Author: "tom"}
@@ -82,8 +140,16 @@ func main() {
 	post2.Create()
 	post3.Create()
 	post4.Create()
-	p, _ := Posts(3)
-	for _, x := range p {
-		fmt.Println(x)
-	}
+	c1 := Comment{Content: "Good Post", Author: "Joe", P: &post}
+	c1.Create()
+	c2 := Comment{Content: "Good Post", Author: "jerry", P: &post2}
+	c2.Create()
+	c3 := Comment{Content: "Good Post", Author: "bob", P: &post3}
+	c3.Create()
+	rd, _ := GetPost(4)
+	fmt.Println(rd)
+	fmt.Println(rd.Comments)
+	fmt.Println()
+	fmt.Println(rd.Comments[0].P)
+	fmt.Println(rd.Comments[0].Author)
 }
