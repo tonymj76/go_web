17fb9ea581df656e9ff25d5721388a98acdc4b9e
Anthony <demeyortj@gmail.com>
2018-12-16 12:42:05 +0100

    web service test with testmain

--- pr11_web_service/web_service_test.go
+++ pr11_web_service/web_service_test.go
@@ -1,28 +1,67 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"encoding/json"
 	"net/http/httptest"
 	"net/http"
 	"testing"
 )
 
+var (
+	mux	*http.ServeMux
+	w		*httptest.ResponseRecorder
+)
+func setUp() {
+	mux = http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+	w = httptest.NewRecorder()
+}
+
 func TestWebServer(t *testing.T) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/post/", handleRequest)	
-	write := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/post/6", nil)
-	mux.ServeHTTP(write, request)
-	if write.Code != 200 {
-		t.Errorf("connection broken %v \n", write.Code)
+	r, _ := http.NewRequest("GET", "/post/6", nil)
+	mux.ServeHTTP(w, r)
+	if w.Code != 200 {
+		t.Errorf("connection broken %v \n", w.Code)
 	}
 
 	var post Post
-	err := json.Unmarshal(write.Body.Bytes(), &post)
+	err := json.Unmarshal(w.Body.Bytes(), &post)
 	if err != nil {
 		t.Error(err)
 	}
-	if post.Author != "jerry" {
+	if post.Author != "Tony" {
 		t.Error("Cannot retrieve Json post")
 	}
+}
+
+func TestHandlePut(t *testing.T) {
+	json := strings.NewReader( `{"content":"write put request", "author":"Tony"}`)
+	r, err := http.NewRequest("PUT", "/post/6", json)
+	if err != nil{
+		t.Error(err)
+	}
+	mux.ServeHTTP(w, r)
+	if w.Code != 200 {
+		t.Error("not successful ", w.Code)
+	}
+}
+
+func TestHandlePost(t *testing.T) {
+	json := strings.NewReader( `{"content":"write post request", "author":"thomson"}`)
+	r, _ := http.NewRequest("POST", "/post/", json)
+	mux.ServeHTTP(w, r)
+	if w.Code != 200 {
+		t.Error("not successful")
+	}
+	if w.Header().Get("Content-Type") != "application/json"{
+		t.Error(w.Header().Get("Content-Type"))
+	}
+}
+
+func TestMain(m *testing.M) {
+	setUp()
+	code := m.Run()
+	os.Exit(code)
 }
